832fc1f5982cf7645911cf30f6d939ff52a9d7ae
Rauf Özdemir <40164810+Raufozdemir@users.noreply.github.com>
2024-12-13 01:09:15 +0300

    Add files via upload

---
+++ metos/src/main/java/com/example/metos/MetosApplication.java
@@ -0,0 +1,13 @@
+package com.example.metos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetosApplication.class, args);
+    }
+
+}
